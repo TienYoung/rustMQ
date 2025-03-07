5aa8f97868fe5e16ae7b37d3ff5f6ecfad941429
Aska <constantine.sky@hotmail.com>
2025-03-07 17:31:58 -0500

    Create a message key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate libc;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        let message_key = libc::ftok(".".as_ptr() as *const i8, 'M' as i32);
+        if message_key == -1 {
+            let errno = *libc::__error();
+            let error_message = io::Error::from_raw_os_error(errno).to_string();
+            eprint!("ftok failed: {}", error_message);
+        }
+        else {
+            println!("message_key: {}", message_key);
+        }
+    }
 }
