864692fa195aa8873202bc479ad672174e9dd007
Aska <constantine.sky@hotmail.com>
2025-03-07 17:50:24 -0500

    Fix error handling for ftok on macOS and other platforms

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ fn main() {
     unsafe {
         let message_key = libc::ftok(".".as_ptr() as *const i8, 'M' as i32);
         if message_key == -1 {
+            #[cfg(target_os = "macos")]
             let errno = *libc::__error();
+            #[cfg(not(target_os = "macos"))]
+            let errno = *libc::__errno_location();
             let error_message = io::Error::from_raw_os_error(errno).to_string();
-            eprint!("ftok failed: {}", error_message);
+            eprintln!("ftok failed: {}", error_message);
         }
         else {
             println!("message_key: {}", message_key);
