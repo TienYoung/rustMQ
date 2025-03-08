845afb8323dc701492b7a9986e052a63e54a6ae2
Aska <constantine.sky@hotmail.com>
2025-03-07 19:13:47 -0500

    Refactor error handling to use platform-specific errno functions

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,33 @@ extern crate libc;
 use std::{io, process};
 use std::ffi::CString;
 
-fn handle_error(message: &str) {
-    #[cfg(target_os = "macos")]
-    let errno = unsafe{ *libc::__error() };
-    #[cfg(not(target_os = "macos"))]
-    let errno: i32 = unsafe { *libc::__errno_location() };
-    let error_message: String = io::Error::from_raw_os_error(errno).to_string();
-    eprintln!("{}: {}", message, error_message);
+#[cfg(target_os = "macos")]
+fn errno() -> io::Error {
+    io::Error::from_raw_os_error(unsafe { *libc::__error() })
+}
+
+#[cfg(target_os = "linux")]
+fn errno() -> io::Error {
+    io::Error::from_raw_os_error(unsafe { *libc::__errno_location() })
 }
 
 fn main() {
     unsafe {
         let path = CString::new(".").expect("CString::new failed");
-        let message_key = libc::ftok(path.as_ptr() as *const i8, 'M' as i32);
+        let message_key: libc::key_t = libc::ftok(path.as_ptr() as *const i8, 'M' as i32);
         if message_key == -1 {
-            handle_error("Cannot create key");
+            eprintln!("Cannot create key: {}", errno());
             process::exit(1);
         }
 
-        let message_id = libc::msgget(message_key, libc::IPC_CREAT | 0o660);
-        if message_id == -1 {
-            handle_error("Cannot create message queue");
+        let message_qeueue_id: libc::c_int = libc::msgget(message_key, libc::IPC_CREAT | 0o660);
+        if message_qeueue_id == -1 {
+            eprintln!("Cannot create message queue: {}", errno());
             process::exit(2);
         }
+
+        println!("Message queue ID: {}\n", message_qeueue_id);
+
+        
     }
 }
